985cc89dc619495c590ab28d61707d83cda884dd
ketan71356 <ketan.kumar@sigmoidanalytics.com>
2022-09-13 01:45:06 +0530

    First commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/animation.dart';
+
+class AppColors{
+
+  static final Color textColor = const Color(0xff070707);
+  static final Color mainColor = const Color(0xffFFC300);
+}
